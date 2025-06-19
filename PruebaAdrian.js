e790fc62b4fa2242b6c45e158f5426269a097c31
AdrianRp7 <parc769@gmail.com>
2025-06-19 10:18:23 +0200

    feat: technical test resolved

---
+++ PruebaAdrian.js
@@ -0,0 +1,108 @@
+//Prueba entregada por Adrián Ródenas Picó el día 19/06/2025
+
+const categories = [
+    {
+        name: 'category1',
+        subcategories: [
+            {
+                name: 'category2',
+                subcategories: []
+            },
+            {
+                name: 'category3',
+                subcategories: [
+                    {
+                        name: 'category4',
+                        subcategories: []
+                    },
+                ]
+            }
+        ]
+    },
+    {
+        name: 'category5',
+        subcategories: []
+    }
+];
+
+//Español
+//Esta solución, a parte de resolver lo que plantea el ejercicio, también resuelve el problema de cuando una categoría se encuentra en más
+//de un lugar, por ejemplo si la categoría 4 esta en otra parte u dentro de la categoría cuatro, en ese caso devuelve un array con todas las ocurrencias
+//de esa categoría. Luego devuelve un string con cada ocurrencia separada por comas y "\n". (Debajo hay datos de ejemplo con este caso)
+
+//Inglés
+//This solution, besides solving what the exercise proposes, also solves the problem when a category is found in more than one place. 
+//For example, if category 4 is elsewhere or within category 4, in that case, it returns an array with all occurrences of that category. 
+//Then, it returns a string with each occurrence separated by commas and "\n" (Below are example data for this case)
+
+// TO-DO: Implement this function
+const getCategoryPath = (categories, categoryName) => {
+    const getArrayCategoryPaths = (categories, categoryName) => {
+        let paths = [];
+        for(let {name, subcategories} of categories) {
+            if(subcategories.length > 0) {
+                let result = getArrayCategoryPaths(subcategories, categoryName);
+                if(result.length !== 0) {
+                    paths.push(...result.map(path => (`/${name}${path}`)))
+                }
+            }
+            if(name === categoryName) {
+                paths.push(`/${name}`);
+            }
+        }
+        return paths
+    }
+
+    const result = getArrayCategoryPaths(categories, categoryName).join(",\n");
+
+    return result.length !== 0 ? result : `Not found ${categoryName}`;
+};
+
+// OUTPUT SAMPLES
+console.log(getCategoryPath(categories, 'category4')); // should output: '/category1/category3/category4'
+console.log(getCategoryPath(categories, 'category2')); // should output: '/category1/category2'
+console.log(getCategoryPath(categories, 'category5')); // should output: '/category5'
+console.log(getCategoryPath(categories, 'category10')); // should output: ''
+
+// const categories = [
+//     {
+//         name: 'category1',
+//         subcategories: [
+//             {
+//                 name: 'category2',
+//                 subcategories: []
+//             },
+//             {
+//                 name: 'category3',
+//                 subcategories: [
+//                     {
+//                         name: 'category4',
+//                         subcategories: []
+//                     },
+//                 ]
+//             }
+//         ]
+//     },
+//     {
+//         name: 'category4',
+//         subcategories: [
+//             {
+//                 name: 'category4',
+//                 subcategories: []
+//             },
+//             {
+//                 name: 'category2',
+//                 subcategories: [
+//                     {
+//                         name: 'category4',
+//                         subcategories: []
+//                     },
+//                 ]
+//             },
+//         ]
+//     },
+//     {
+//         name: 'category5',
+//         subcategories: []
+//     }
+// ];
